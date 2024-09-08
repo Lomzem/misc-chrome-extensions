f71d7e21b4b32df2579efd6a88c2dd25f45919bc
Lawjay Lee <lawjaylee1@icloud.com>
2024-09-08 12:31:43 -0700

    chico canvas

--- canvas-autologin/canvas-autologin.js
+++ canvas-autologin/canvas-autologin.js
@@ -1,8 +1,8 @@
 (function() {
     'use strict';
-    const user = document.getElementById("frmLogin_UserName");
-    const login = document.getElementById("btnLogin");
-    const pwd = document.getElementById("frmLogin_Password");
+    const user = document.getElementById("username");
+    const login = document.getElementsByName("_eventId_proceed")[0];
+    const pwd = document.getElementById("password");
 
     function userFilled() {
         if (user.value.length == 0 || pwd.value.length == 0) {
